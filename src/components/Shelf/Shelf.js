7e3634f1f28fa7713b7a7a1cb294dbecf07aef09
steve hanlon <rshanlon@gmail.com>
2018-10-06 23:44:18 -0700

    Move static HTML and CSS into component files

--- src/components/Shelf/Shelf.js
+++ src/components/Shelf/Shelf.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Shelf.css';
+import Book from '../Book/Book';
+
 
 class Shelf extends Component {
   render() {
     return (
-
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">{this.props.name}</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <Book />
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
     );
   }
 }
 
 Shelf.propTypes = {
-  
+
 }
 
 export default Shelf;
